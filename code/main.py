e75dd12638e2e3f5fa305ebb35ac4b7316c8f8b8
Daniela Nino <danielanino194@gmail.com>
2023-09-10 20:56:11 -0500

    Calculated execution time in main

--- code/main.py
+++ code/main.py
@@ -2,17 +2,19 @@ from xlwt import Workbook
 from input_data import read_data
 from output_data import write_data
 from algorithm1 import algorithm1
-
+from time import time
 
 wb0 = Workbook()
 # wb1 = Workbook()
 # wb2 = Workbook()
 
-for instance in range(1,4):
-    
-    n,m,time,order = read_data(instance)
-    sol,Z,f = algorithm1(n,m,time,order)
-    write_data(wb0, instance, sol, Z, f)
+for instance in range(1,17):
+    n,m,timep,order = read_data(instance)
+    t1 = time()
+    sol,Z = algorithm1(n,m,timep,order)
+    t2 = time()
+    t = t2 - t1
+    write_data(wb0, instance, sol, Z, t)
 
 wb0.save("JSSP_Daniela_Nino_1.xls")
 # wb1.save("JSSP_Daniela_Nino_2.xls")
