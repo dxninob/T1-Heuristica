68448883da409e0c67260eaab2abb822e52c59fb
Daniela Nino <danielanino194@gmail.com>
2023-09-27 21:22:29 -0500

    Insertion Right First Improvement

---
+++ neighborhoods/Insertion_Right_First_Improvement.py
@@ -0,0 +1,35 @@
+import numpy as np
+from methods import feasible, time_Z
+
+# Revisar si debo hacer copias de un objeto
+def IRFI(sol, Z, timep, order, jobs, machines):
+    stop = False
+    while not stop:
+        stop = True
+        for m in range(machines):
+            for j in range(jobs):
+                for i in range(j+1, jobs):
+                    sol_neighbor = move(sol, m, j, i)
+                    if feasible(sol_neighbor, order, jobs, machines):
+                        Z_neighbor = time_Z(sol_neighbor, timep, order, jobs, machines)
+                        if Z_neighbor < Z:
+                            stop = False
+                            sol = sol_neighbor
+                            Z = Z_neighbor
+                            break
+                if not stop:
+                    break
+            if not stop:
+                break
+        # Es necesario este?
+        if not stop:
+            break
+    return sol, Z
+
+
+def move(sol, m, j, i):
+    new_sol = np.copy(sol)
+    for k in range(j+1,i+1):
+        new_sol[m][k-1] = new_sol[m][k]
+    new_sol[m][i] = sol[m][j]
+    return new_sol
