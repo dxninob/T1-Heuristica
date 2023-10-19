d638e0acb87def9cb626edb2f63c4a86ec8833b6
Daniela Nino <danielanino194@gmail.com>
2023-10-18 20:42:29 -0500

    Added VND

---
+++ metaheuristic/VND.py
@@ -0,0 +1,41 @@
+import numpy as np
+import sys
+sys.path.append('./intensification')
+from methods import feasible, time_Z
+# from Insertion_Backward_Best_Improvement import IBBI
+from Insertion_Backward_First_Improvement import IBFI
+# from Insertion_Forward_Best_Improvement import IFBI
+from Insertion_Forward_First_Improvement import IFFI
+# from Interchange_Best_Improvement import IBI
+from Interchange_First_Improvement import IFI
+from time import time
+
+
+def VND(sol, Z, timep, order, jobs, machines, time_limit):
+    t1 = time()
+    current_time = 0
+    num_neighborhoods = 3
+    neighborhood = 1
+    
+    while current_time < time_limit and neighborhood <= num_neighborhoods:
+        if neighborhood == 1:
+            # Insertion Forward
+            sol_neighbor, Z_neighbor = IFFI(sol, Z, timep, order, jobs, machines)
+        elif neighborhood == 2:
+            # Insertion Backward
+            sol_neighbor, Z_neighbor = IBFI(sol, Z, timep, order, jobs, machines)
+        elif neighborhood == 3:
+            # Interchange Forward
+            sol_neighbor, Z_neighbor = IFI(sol, Z, timep, order, jobs, machines)
+        
+        if Z_neighbor < Z:
+            neighborhood = 1
+            sol = sol_neighbor
+            Z = Z_neighbor
+        else:
+            neighborhood += 1
+        
+        t2 = time()
+        current_time = t2 - t1
+
+    return sol, Z
---
+++ metaheuristic/main.py
@@ -0,0 +1,37 @@
+import sys
+sys.path.append('./algorithms')
+from xlwt import Workbook
+from input_data import read_data
+from output_data import write_data
+from algorithm1 import constructive
+from VND import VND
+from time import time
+import numpy as np
+
+
+time_limit = {"JSSP1": 2, "JSSP2": 2, "JSSP3": 2, "JSSP4": 2, "JSSP5": 75, "JSSP6": 75, "JSSP7": 75, "JSSP8": 75, "JSSP9": 75, "JSSP10": 75, "JSSP11": 150, "JSSP12": 150, "JSSP13": 300, "JSSP14": 300, "JSSP15": 1800, "JSSP16": 1800}
+wbvnd = Workbook()
+wbr = Workbook()
+sheetwbr = wbr.add_sheet(f'Results')
+wbt = Workbook()
+sheetwbt = wbt.add_sheet(f'Time')
+
+for instance in range(1,6):
+    print(f"Instance{instance}")
+    n,m,timep,order = read_data(instance)
+
+    sol, Z = constructive(n,m,timep,order)
+    print("constructive:", Z)
+
+    t1 = time()
+    new_sol, new_Z = VND(sol, Z, timep, order, n, m, time_limit[f'JSSP{instance}'])
+    print("VND:", new_Z)
+    t2 = time()
+    t = t2 - t1
+    write_data(wbvnd, instance, new_sol, new_Z, t)
+    sheetwbr.write(1, instance, int(new_Z))
+    sheetwbt.write(1, instance, t)
+
+wbvnd.save("JSSP_Daniela_Nino_VND.xls")
+wbr.save("Results.xls")
+wbt.save("Time.xls")
